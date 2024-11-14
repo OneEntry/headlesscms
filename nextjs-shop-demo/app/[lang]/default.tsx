835e0c49317e5c926a10c64a084b2a4f2d1d0594
OneEntry <gradov@oneentry.ae>
2024-11-14 11:55:12 +0400

    update

--- nextjs-shop-demo/app/[lang]/default.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default Layout;
--- nextjs-shop-demo/app/types/env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@env' {
-  export const PROJECT_URL: string;
-  export const APP_TOKEN: string;
-}
